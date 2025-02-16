74c5f628c61866737af44618f05a340036ba8ebe
AhmedRadwan17 <marimradwan922@gmail.com>
2025-02-17 00:26:19 +0200

    Operators

---
+++ Operators.py
@@ -0,0 +1,13 @@
+num1=int(input("enter num1: "))
+num2=int(input("enter num2: "))
+Operator=input("enter Operator: (+,-,*,/,%) ")
+if Operator=="+" :
+    print(f"Num1+Num2:  {num1+num2}")
+elif Operator=="-" :
+    print(f"Num1-Num2: {num1-num2}")
+elif Operator=="*" :
+    print(f"Num1*Num2: {num1*num2}")
+elif Operator=="/" :
+        print(f"Num1/Num2: {num1/num2}")
+elif Operator=="%" :
+    print(f"Num1%Num2: {num1%num2}")
