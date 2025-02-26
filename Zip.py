dc671d42ae5d6d161c889df42fbfe78be570be08
Ahmed Radwan <marimradwan922@gmail.com>
2025-02-26 15:39:22 +0200

    zip.py

--- Zip.py
+++ Zip.py
@@ -1,9 +1,11 @@
 # zip()
 list1 = [1,2,3,4,5]
 list2 = ["A","B","C"]
+dict1 = {"Name":"Ahmed","Age":22}
 y = zip(list1,list2)
 for x in y :
     print(x)
-for item1 , item2 in zip(list1,list2):
+for item1 , item2 , item3 in zip(list1,list2,dict1):
     print(f"list 1 item: {item1}")
     print(f"list 2 item: {item2}")
+    print(f"list 3 Key : {item3} and value : {dict1[item3]}")
