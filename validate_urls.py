d654b942712f91518024ea7fd6cea174c0ac92ba
Ahmed Radwan <marimradwan922@gmail.com>
2025-03-01 01:49:30 +0200

    validate_urls.py

---
+++ validate_urls.py
@@ -0,0 +1,30 @@
+import re
+
+readmore = 'y'
+list1 = []
+
+while readmore.lower() == 'y':
+    
+    My_String = input("Enter your URL: ")
+    Check = re.findall(r'^(https?://)?(www\.)?[\w-]+\.\w+(\.net)?$', My_String)
+
+    if Check:
+        print("Valid URL!,",end=" ")
+    else:
+        print("Invalid URL!")
+
+    if Check:
+        list1.append(My_String)
+        print("Added Successfully!")
+        readmore = input("Do you want to add emails: ")
+    else:
+        print("Not Valid")
+        readmore = 'n'
+counter=1     
+if readmore.lower() != 'y':
+    print("End Add,",end=" ")
+    if list1:
+        print("Emails in Database are:")
+        for email in list1:
+            print(f"({counter}) {email}")
+            counter +=1
