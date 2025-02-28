e2cd6a2f93e5c2e3cda3ac6dc011114b3114170c
Ahmed Radwan <marimradwan922@gmail.com>
2025-02-28 22:02:10 +0200

    python_pythex.py

--- python_pythex.py
+++ python_pythex.py
@@ -1,18 +1,30 @@
 """
 \d (Degit)
 \w (char + Numbers)
+
 \s (spaces)
 \w{1} \w{3} 
+
 \w{1,4} \w{1,} 
 ( How To Mark Number )
+
 \d{3}\s\d{4}\s\d{4}
 [A-Z] All char from A To Z
+
 [^A-Z] Not All char from A To Z
 \d{3}\s\d{4}-?\s?\d{3} (Number)
+
 ? remain that if exist ok not exist ok 
+
 To Make mark start from start of line using (^)
 To Make mark end without anything using ($)
+
 (Os33ma_Elzero@gamil.com
 Os33ma.Elzero@gamil.com)
 [A-z0-9\.]+@+[A-z0-9]+\.+[A-z]+
+
++=Os33ma_Elzero@gamil.com
+Os33ma.Elzero@gamil.com
+Os33ma.Elzero@gamil.net
+^[A-z0-9\.]+@+[A-z0-9]+\.+[A-z]+$
 """
