67ee984402752c85e8be66b77ce72758fb61871d
Ahmed Radwan <marimradwan922@gmail.com>
2025-03-01 15:18:15 +0200

    pythex_explain.py

--- pythex_explain.py
+++ pythex_explain.py
@@ -31,6 +31,55 @@ Os33ma.Elzero@gamil.net
 To specify The end of Mark of email
 \.(com|net)
 
+"""
+\d (Degit)
+\w (char + Numbers)
+
+\s (spaces)
+\w{1} \w{3} 
+
+\w{1,4} \w{1,} 
+( How To Mark Number )
+
+\d{3}\s\d{4}\s\d{4}
+[A-Z] All char from A To Z
+
+[^A-Z] Not All char from A To Z
+\d{3}\s\d{4}-?\s?\d{3} (Number)
+
+? remain that if exist ok not exist ok 
+
+To Make mark start from start of line using (^)
+To Make mark end without anything using ($)
+
+(Os33ma_Elzero@gamil.com
+Os33ma.Elzero@gamil.com)
+[A-z0-9\.]+@+[A-z0-9]+\.+[A-z]+
+
++=Os33ma_Elzero@gamil.com
+Os33ma.Elzero@gamil.com
+Os33ma.Elzero@gamil.net
+^[A-z0-9\.]+@+[A-z0-9]+\.+[A-z]+$
+
+To specify The end of Mark of email
+\.(com|net)
+
+(\d-|\d\)|\d>)\s?[A-z]+
+1- CPP
+1) HTML
+1>PHP
+
+\d{3}\s\d{4}\s(\d{3}|\(\d{3}\))
+012 3773 838
+012 8383 (778)
+
+^(https?://)(www)?([A-z])+\.+(\w)+(\.(net))?$
+http://www.elzero.net
+http://elzero.org
+https://elzerocourses.com
+https://elzerocourses.size
+Hello Osama Elzero
+
 (\d-|\d\)|\d>)\s?[A-z]+
 1- CPP
 1) HTML
@@ -49,4 +98,13 @@ Hello Osama Elzero
 
 Link Of pythex python 
 https://pythex.org/
+
+(https?)://(www\.)?(\w+)\.(\w+)(:\d+)?(/)?(.+)
+https://www.elzero.org:8080/category.php?article=105?name=how-to-do
+https://www.elzero.org/category.php?article=105?name=how-to-do
+https://elzero.org/category=50?article=105?name=how-to-do
+http://elzero.org/category=50?article=105?name=how-to-do
+http://elzero.org
+http://elzero.net
+
 """
